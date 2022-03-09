f8ef85927a31974e4f4299a00f120d30dcfd3628
techLeadAtLogicOverCode <techLead@logicovercode.com>
2022-03-09 11:50:08 +0530

    spring dependencies refactored

--- commonly-used/dependencies/SpringDependencies.scala
+++ commonly-used/dependencies/SpringDependencies.scala
@@ -7,76 +7,61 @@ trait SpringDependencies {
 
   case class SpringBootVersion(value: String)
 
-  def springCore(version: String = "4.3.2.RELEASE"): FSbtModuleId = {
-    "org.springframework" % "spring-context" % version
+  trait StarterVersion {
+    val releaseTag: SpringBootVersion
   }
 
-  val springBoot_2_3_0 = SpringBootVersion("2.3.0.RELEASE")
-
-  @Deprecated
-  val springBootVersion_2_3_0 = SpringBootVersion("2.3.0.RELEASE")
-
-  @Deprecated
-  val springBootVersion_2_2_5 = SpringBootVersion("2.2.5.RELEASE")
-
-  case class SpringBootDependencies(releaseTag: SpringBootVersion) {
-
-    def starterParent(): FSbtModuleId = {
+  trait StarterParent extends StarterVersion {
+    def starter_parent(): FSbtModuleId = {
       ("org.springframework.boot" % "spring-boot-starter-parent" % releaseTag.value).pomOnly()
     }
+  }
 
-    def starterWeb(): FSbtModuleId = {
+  trait StarterWeb extends StarterVersion {
+    def starter_web(): FSbtModuleId = {
       "org.springframework.boot" % "spring-boot-starter-web" % releaseTag.value
     }
+  }
 
-    def starterDataJpa(): FSbtModuleId = {
+  trait StarterDataJpa extends StarterVersion {
+    def starter_data_jpa(): FSbtModuleId = {
       "org.springframework.boot" % "spring-boot-starter-data-jpa" % releaseTag.value
     }
+  }
 
-    def starterSecurity(): FSbtModuleId = {
+  trait StarterSecurity extends StarterVersion {
+    def starter_security(): FSbtModuleId = {
       "org.springframework.boot" % "spring-boot-starter-security" % releaseTag.value
     }
+  }
 
-    def starterJdbc(): FSbtModuleId = {
+  trait StarterJdbc extends StarterVersion {
+    def starter_jdbc(): FSbtModuleId = {
       "org.springframework.boot" % "spring-boot-starter-jdbc" % releaseTag.value
     }
+  }
 
-    def starterTest(): FSbtModuleId = {
+  trait StarterTest extends StarterVersion {
+    def starter_test(): FSbtModuleId = {
       "org.springframework.boot" % "spring-boot-starter-test" % releaseTag.value
     }
+  }
 
-    def postgres(): FSbtModuleId = {
-      "org.postgresql" % "postgresql" % "42.2.12"
-    }
-
-    def flywayCore(): FSbtModuleId = {
-      "org.flywaydb" % "flyway-core" % "6.4.1"
+  trait StarterActuator extends StarterVersion {
+    def starter_actuator(): FSbtModuleId = {
+      "org.springframework.boot" % "spring-boot-starter-actuator" % releaseTag.value
     }
   }
 
-  val springCloudGreenWichRelease = "Greenwich.RELEASE"
-  val latestSpriingCloudReleaseTag = springCloudGreenWichRelease
-
-  case class SpringCloud(
-      releaseTag: SpringBootVersion = springBootVersion_2_2_5
-  ) {
-
-    val versionsMap = Map("Greenwich.RELEASE" -> "2.1.0.RELEASE")
-
-    def dependencies(): FSbtModuleId = {
-      ("org.springframework.cloud" % "spring-cloud-dependencies" % releaseTag.value).pomOnly()
-    }
-
-    def starterOAuth2(): FSbtModuleId = {
-      "org.springframework.cloud" % "spring-cloud-starter-oauth2" % versionsMap(
-        releaseTag.value
-      )
-    }
-
-    def starterSecurity(): FSbtModuleId = {
-      "org.springframework.cloud" % "spring-cloud-starter-security" % versionsMap(
-        releaseTag.value
-      )
-    }
+  val springBoot_2_6_4 = new StarterParent
+    with StarterWeb
+    with StarterDataJpa
+    with StarterSecurity
+    with StarterJdbc
+    with StarterTest
+    with StarterActuator {
+    override val releaseTag: SpringBootVersion = SpringBootVersion("2.6.4")
   }
+
+  val springBootLatestStableRelease = springBoot_2_6_4
 }
